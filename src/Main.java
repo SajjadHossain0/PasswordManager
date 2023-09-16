40667fac1f21f82a46d77249aaa65f16031823cb
Clock <mdsajjadhossain614@gmail.com>
2023-09-16 15:19:55 +0100

    ...

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,8 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args){
-        try{
-            // this is connection for (database mysql)
-            //Connection connection = ConnectionProvider.getConnection();
-
             entry entry = new entry();
             //login login = new login();
             //mainField mainField = new mainField();
-
-        }catch (Exception e){
-            System.out.println(e);
-        }
     }
 }
